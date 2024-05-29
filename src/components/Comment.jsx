599bf0493fb10bf4221bca741d0165cccabc0773
9Brunodox <bruno.moreiradasilva.2002@gmail.com>
2024-05-28 22:25:18 -0300

    Project in JavaScript finish

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,24 @@
 import styles from './Comment.module.css';
 import Brunodox from '../assets/perfil1.jpg'
+import { useState } from 'react'
 import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 
-export function Comment({ content }){
+export function Comment({ content, onDeleteComment }){
+
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment(){
+        onDeleteComment(content);
+    }
+
+    function handleLikeComment(){
+        setLikeCount((state) => {
+            return state + 1;
+        });
+    }
+
+
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={false} src={Brunodox} />
@@ -16,16 +31,16 @@ export function Comment({ content }){
                             <time title="11 de maio às 08:13h" dateTime="2024-05-25 09:13:30">Cerca de 1 hora atrás</time>
                         </div>
 
-                        <button title="Deletar comentário">
-                            <Trash />
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
+                            <Trash size={20} />
                         </button>
                     </header>
                     <p>{content}</p>
                 </div>
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp/>
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
